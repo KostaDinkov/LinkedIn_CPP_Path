b6724e90358142bff8fec7bf1c810ec805dc0317
Kosta Dinkov <k.v.dinkov@gmail.com>
2022-08-25 00:14:30 +0300

    add: Chapter one exercise

---
+++ course_1/01_01/01_01/01_01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+
+int main()
+{
+    std::string str;
+    std::cout << "Enter name!" << std::endl;
+    std::cin >> str;
+    std::cout << "Hello " << str << std::endl;
+    return 0;
+}
+
